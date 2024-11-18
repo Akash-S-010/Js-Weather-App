00015c549e90f043e3b4420ed9be26429c490945
Akash S <akashspalloor@gmail.com>
2024-11-18 11:20:23 +0530

    implemented videos of weather

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let humidity = document.querySelector(".humidity");
 let wind = document.querySelector(".wind");
 let tempContainer = document.querySelector(".weather-info");
 let humidityContainer = document.querySelector(".bottom-div");
-
+let video = document.querySelector("video source");
 
 const apiKey = "84210a5d9d02fa72e8739474b49ca8f1";
 
@@ -38,20 +38,32 @@ search.addEventListener("click", () => {
 
             //------ Arrange the cloud image as per weather
             if (cloud === "Clouds") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/854002/854002-hd_1920_1080_24fps.mp4";
                 weatherImage.src = "images/cloud.png";
             } else if (cloud === "Clear") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/29357417/12651084_640_360_30fps.mp4";
                 weatherImage.src = "images/clear.png";
             } else if (cloud === "Rain") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/1841455/1841455-hd_1280_720_25fps.mp4";
                 weatherImage.src = "images/rain.png";
             } else if (cloud === "Snow") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/4419948/4419948-hd_1920_1080_24fps.mp4";
                 weatherImage.src = "images/snow.png";
             } else if (cloud === "Mist") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/3615892/3615892-uhd_2560_1440_25fps.mp4";
                 weatherImage.src = "images/mist.png";
             } else if (cloud === "Haze") {
+                video.parentElement.load()
+                video.src = "https://videos.pexels.com/video-files/854752/854752-hd_1920_1080_30fps.mp4";
                 weatherImage.src = "images/haze.png";
             }
 
-
+            // --fetched data shown in app-------
             temperature.innerHTML = `${tempVal}<span>°C</span>`;
             weatherPara.innerHTML = description;
             humidity.innerHTML = `${humidityVal} %`;
@@ -59,7 +71,6 @@ search.addEventListener("click", () => {
 
         } catch (error) {
             console.error(error);
-
 
             tempContainer.style.display = "block";
             humidityContainer.style.display = "none";
